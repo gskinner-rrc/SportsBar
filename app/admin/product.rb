1b45b1abbd1160a8b9bd984c815ca50afdfdff99
Graeme Skinner <gskinner204@gmail.com>
2014-04-01 09:56:49 -0500

    attempted tweaking carrierwave settings still with limited success

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,6 +1,19 @@
 ActiveAdmin.register Product do
-
   
+permit_params :name, :description, :price, :stock_quantity, :category_id
+
+form(:html => { :multipart => true }) do |f|
+  f.inputs "Product" do
+    f.input :name
+    f.input :description
+    f.input :price
+    f.input :stock_quantity
+    f.input :category_id
+    f.input :image_filename, :as => :file
+
+  end
+  f.action :submit
+end
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
