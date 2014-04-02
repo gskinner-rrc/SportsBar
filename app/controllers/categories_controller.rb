50806b17bae6507d44e6212f5ecd0468806c9048
Graeme Skinner <gskinner204@gmail.com>
2014-04-01 21:01:29 -0500

    Added an association between products and categories and added validations

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,26 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all
   end
+  #Loads up the view of the same name after the action has executed
+  # app/views/products/index.html.erb
+  # This view will have access to the @products variable.
 
   def show
-  end
+    @category = Category.find(params[:id])
+  end # Loads: app/views/products/show.html.erb
 
   def new
-  end
+    @category = Category.new
+  end # Loads: app/views/products/new.html.erb
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to action: :index
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +30,10 @@ class CategoriesController < ApplicationController
   end
 
   def destroy
+  end
+  
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ActiveRecord::Base
+  belongs_to :category
+  
   mount_uploader :image_filename, ImageUploader
+  
+  validates :name, :description, :price, :stock_quantity, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :stock_quantity, numericality: { only_integer: true }
 end
